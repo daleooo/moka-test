10f0f6f2844f20206f9d87b6ef723ec217f890e3
dale <daleoooo.z@gmail.com>
2015-11-15 14:07:10 +0800

    Toggle jobs checkbox completed & Toggle departments checkbox completed & Clear selected items completed

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -22,67 +22,217 @@ var getJobsByDepartmentId = function (id) {
   return jobs;
 }
 
+var serverGetDepartments = function () {
+  return departmentsMock.map(function (department) {
+    return department;
+  });
+}
+
+var serverGetJobsByDepartmentId = function (id) {
+  return getJobsByDepartmentId(id);
+}
+
+var serverDeleteJobById = function (id) {
+  var jobs = jobsMock;
+  jobsMock = jobs.filter(function (job) {
+    return (job.id != id); 
+  })
+
+  console.log("Delete Job " + id);
+  console.log(jobsMock);
+}
+
 var JobRow = React.createClass({
   render: function() {
     console.log("JobRow render");
 
     var job = this.props.job;
     return(
-      <tr className="jobRow">
-      <td></td>
-      <td><input type="checkbox" /></td>
-      <td>{job.name}</td>
-      <td>{job.quantity}</td>
-      </tr>
+      <div className="jobRow">
+      <input type="checkbox" className="jobCheckbox" checked={job.checked} value={job.id} />
+      <div>{job.name}</div>
+      <div>{job.quantity}</div>
+      </div>
     );
   }
 });
 
 var JobTable = React.createClass({
+  getInitialState: function() {
+    return {
+      jobs: [],
+    };
+  },
+
+  updateJobs: function () {
+    var department = this.props.department,
+      id = department.id;
+
+    /* dynamic load here  */
+    var jobs = serverGetJobsByDepartmentId(id);
+    console.log(jobs);
+    this.setState({jobs: jobs.map(function (job) {
+      job.checked = false; 
+      return job;
+    })});
+  },
+
+  componentWillMount: function () {
+    console.log("Job Table cWm");
+    this.updateJobs();
+  },
+
+  componentWillReceiveProps: function () {
+    console.log("Job Table wrp");
+    this.updateJobs();
+  },
+
+  handleJobStateChange: function (e) {
+    var t = e.target,
+      that = this;
+
+    var _setJobsState = function (checked, id) {
+      if (id === undefined) {
+        this.setState(function (previousState, currentProps) {
+          return previousState.jobs.map(function (job) {
+              job.checked = checked;
+              that.props.onJobStateChange(checked, job.id);
+            return job;
+          }); 
+        });
+      }
+      else {
+
+        this.setState(function (previousState, currentProps) {
+          return previousState.jobs.map(function (job) {
+            if (job.id == id) {
+              job.checked = checked;
+              that.props.onJobStateChange(checked, job.id);
+            }
+            return job;
+          }); 
+        });
+      }
+    }
+
+    var setJobsState = _setJobsState.bind(this);
+
+    switch (t.className) {
+      case "departmentCheckbox":
+        if (t.checked)
+          setJobsState(true);         
+        else
+          setJobsState(false);         
+        break;
+      case "jobCheckbox":
+        if (t.checked)
+          setJobsState(true, t.value);
+        else
+          setJobsState(false, t.value);
+        break;
+      default:
+        break;
+    }
+
+    e.stopPropagation();
+
+    console.log(this.state.jobs);
+  },
+
   render: function() {
-    console.log("DepartmentBox render");
+    console.log("JobTable render");
+    console.log(this.state.jobs);
 
     var department = this.props.department,
-      jobs = department.jobs,
+      jobs = this.state.jobs,
       quantity = 0;
 
+    var that = this;
     var jobRowNodes = jobs.map(function (job) {
       quantity += job.quantity;
       return (
         <JobRow key={job.id} job={job} />
       );
     });
-
-    return (
-      <div className="jobTable">
-      <p><input type="checkbox" /></p>
-      <p>{department.name}</p>
-      <p>{quantity}</p>
-      <table>
-      <tbody>
-      {jobRowNodes}
-      </tbody>
-      </table> 
-      </div>
-    );
+    
+    if (quantity > 0) {
+      return (
+        <div className="jobTable" onChange={this.handleJobStateChange}>
+        <p><input type="checkbox" className="departmentCheckbox" checked={this.state.jobState} value={department.id}/></p>
+        <p>{department.name}</p>
+        <p>{quantity}</p>
+        {jobRowNodes}
+        </div>
+      );
+    }
+    else {
+      return false;
+    }
   }
 });
 
 var JobBox = React.createClass({
+
+  jobState: {},
+
+  getInitialState: function() {
+    return {
+      departments: [],
+    };
+  },
+
+  componentDidMount: function() {
+
+    /* dynamic load here  */
+    var departments = serverGetDepartments();
+    console.log("Job box cdm");
+    console.log(departments);
+    this.setState({departments: departments});
+  },
+
+  handleJobStateChange: function (checked, id) {
+    console.log(id);
+    if (checked) {
+      if (this.jobState[id] !== undefined) 
+        this.jobState[id]  = true;
+      else
+        this.jobState[id] = true;
+    }
+    else {
+      if (this.jobState[id] !== undefined)
+        this.jobState[id] = false;
+    }
+
+    console.log(this.jobState);
+  },
+
+  handleButtonClick: function () {
+    Object.getOwnPropertyNames(this.jobState).map(function (id) {
+      /* delete job  */                                                 
+      serverDeleteJobById(id);
+    });
+    var departments = serverGetDepartments();
+    this.setState({departments: departments});
+  },
+
   render: function() {
     console.log("JobBox render");
+    console.log(this.state.departments);
+    console.log(departmentsMock);
+    console.log(jobsMock);
 
-    var jobTableNodes = departmentsMock.map(function (department) {
-      department.jobs = getJobsByDepartmentId(department.id);
+    var that = this;
+    var jobTableNodes = this.state.departments.map(function (department) {
+      var id = department.id;
       return (
-        <JobTable key={department.id} department={department} />
+        <JobTable key={department.id} department={department} onJobStateChange={that.handleJobStateChange}/>
       );
     });
 
     return (
       <div className="jobBox">
       <p>招聘职位</p>
-      <p>清空</p>
+      <button onClick={this.handleButtonClick}>清空</button>
       {jobTableNodes}
       </div> 
     );
@@ -95,7 +245,7 @@ var Sidebar = React.createClass({
     console.log("Sidebar render");
     return (
       <div className="sidebar">
-      <JobBox />
+      <JobBox data={departmentsMock} />
       </div>
     );
   }
